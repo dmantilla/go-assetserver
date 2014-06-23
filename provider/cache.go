73a49ad9b0312a5ff4e14c45fcaab713c0d5ac9c
Daniel Mantilla <dan@appcabinet.com>
2014-06-23 15:51:02 -0400

    fixing file permissions

--- provider/cache.go
+++ provider/cache.go
@@ -27,11 +27,11 @@ func (cache CacheProvider) GetFile(fileName string) (file []byte, err error) {
 func (cache CacheProvider) WriteFile(fileName string, data []byte) (error) {
 	fullName := cache.FullFileName(fileName)
 	cache.CreateDirectories(fullName)
-	return ioutil.WriteFile(fullName, data, 0755)
+	return ioutil.WriteFile(fullName, data, 0777)
 }
 
 func (cache CacheProvider) CreateDirectories(fullFileName string) {
-	os.MkdirAll(path.Dir(fullFileName), 0755)
+	os.MkdirAll(path.Dir(fullFileName), 0777)
 }
 
 func (cache CacheProvider) FullFileName(fileName string) string {
